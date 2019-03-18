1974cd41a06fca5a8c1799ef898a631b5930747e
maurotlxd <maurocanaleszapata@gmail.com>
2019-03-18 23:11:54 +0100

    ejemplo Alertify JS

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 				var html = "";
 				html += "<table class='table table-bordered'>"
 				html += "<tr><th>Nombre</th><th>Apellidos</th></tr>"
-				html += "<tr><td>"+datos.nombre+"</td><td>"+datos.apellidos+"</td></tr>"
+				html += "<tr><td>" + datos.nombre + "</td><td>" + datos.apellidos + "</td></tr>"
 				html += "</table>"
 				
 					alertify.alert('Operacion Exitosa', 'Operacion Completada', function(){
